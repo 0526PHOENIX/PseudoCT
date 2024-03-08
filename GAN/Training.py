47e9115812d5fdbf315119f6d3ab312b0b2fb6b0
Phoenix <0526phoenix@gmail.com>
2024-03-08 14:21:39 +0800

    1421

--- GAN/Training.py
+++ GAN/Training.py
@@ -30,6 +30,7 @@ MAX = 10000000
 STRIDE = 5
 BATCH = 64
 EPOCH = 30
+LR = 1e-5
 
 METRICS = 5
 METRICS_GEN = 0
@@ -93,8 +94,8 @@ class Training():
         print('\n' + 'Model Initialized' + '\n')
         
         # Optimizer: Adam
-        self.optimizer_gen = Adam(self.gen.parameters(), lr = 1e-5)
-        self.optimizer_dis = Adam(self.dis.parameters(), lr = 1e-5)
+        self.optimizer_gen = Adam(self.gen.parameters(), lr = LR)
+        self.optimizer_dis = Adam(self.dis.parameters(), lr = LR)
 
         print('\n' + 'Optimizer Initialized' + '\n')
 
